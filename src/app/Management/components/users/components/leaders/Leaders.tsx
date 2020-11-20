46519710b33a4fd54d90f1ba82f1bc716d4b37bc
Nelson Frank <Nelsonfr78@outlook.com>
2020-11-20 10:30:23 +0300

    feat: Add Leader page (#39)
    Fixes #35

---
+++ src/app/Management/components/users/components/leaders/Leaders.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//Components
+import { Divider } from 'antd';
+import { RouteComponentProps } from '@reach/router';
+import LeadersTable from './components/leaders-table';
+import Card from '../../../../../components/card';
+
+// Props Types
+// export interface LeadersProps {}
+
+const Leaders: React.FC<RouteComponentProps> = () => {
+  return (
+    <>
+      <Card title="Leaders" styles={{ minHeight: '90vh' }}>
+        <Divider />
+        <LeadersTable />
+      </Card>
+    </>
+  );
+};
+
+export default Leaders;
