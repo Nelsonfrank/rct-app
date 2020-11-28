210b921689b9004b0660d5fc21396c3af9e886e8
nelsonfrank <nelsonfr78@outlook.com>
2020-11-28 09:42:08 +0300

    feat(component): change navigation component design #56
    Fixes #56

--- src/app/users/components/section-card-list/components/section-body/SectionBody.tsx
+++ src/app/users/components/section-card-list/components/section-body/SectionBody.tsx
@@ -2,12 +2,47 @@ import React from 'react';
 
 //Components
 import CardItem from './components/card-item';
+import { Carousel } from 'antd';
 
 // Props Types
 export interface SectionBodyProps {
   objectList: { name: string; image?: string }[];
 }
 
+// Carousel Setting
+const settings = {
+  dots: false,
+  arrow: false,
+  slidesToShow: 2,
+  initialSlide: 2,
+  autoplay: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 const SectionBody: React.FC<SectionBodyProps> = (props: SectionBodyProps) => {
   const { objectList } = props;
 
@@ -15,14 +50,14 @@ const SectionBody: React.FC<SectionBodyProps> = (props: SectionBodyProps) => {
     <>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
           marginTop: '0.75rem',
         }}
       >
-        {objectList.map((item) => (
-          <CardItem username={item.name} key={item.name} />
-        ))}
+        <Carousel {...settings}>
+          {objectList.map((item) => (
+            <CardItem username={item.name} key={item.name} />
+          ))}
+        </Carousel>
       </div>
     </>
   );
