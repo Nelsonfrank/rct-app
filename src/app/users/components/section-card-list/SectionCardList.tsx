796cc574e774ea81139dd312eaeaead1399a83e6
nelsonfrank <nelsonfr78@outlook.com>
2020-11-29 17:43:30 +0300

    feat(component): add shop by platform section
    Fixes #46

--- src/app/users/components/section-card-list/SectionCardList.tsx
+++ src/app/users/components/section-card-list/SectionCardList.tsx
@@ -2,23 +2,57 @@ import React from 'react';
 
 // Components
 import SectionHeader from './components/section-header';
-import SectionBody from './components/section-body';
+import Card from '../../../components/card';
+import { Button } from 'antd';
+// Styles
+import './SectionCardList.less';
+//Images
+import Slider1 from '../../../../assets/images/slider-1.jpg';
 
 // Props Types
 export interface SectionCardListProps {
   title: string;
   route?: string;
-  objectList: { name: string; image?: string }[];
 }
 
 const SectionCardList: React.FC<SectionCardListProps> = (
   props: SectionCardListProps,
 ) => {
-  const { title, route, objectList } = props;
+  const { title, route } = props;
   return (
     <>
       <SectionHeader title={title} route={route} />
-      <SectionBody objectList={objectList} />
+      <div className="card--list_group">
+        <Card
+          className="card-section_container"
+          img={Slider1}
+          imgTitle="Sarafina John"
+          imgDescrip="Arusha"
+        ></Card>
+        <Card
+          className="card-section_container"
+          img={Slider1}
+          imgTitle="Sarafina John"
+          imgDescrip="Arusha"
+        ></Card>
+        <Card
+          className="card-section_container"
+          img={Slider1}
+          imgTitle="Sarafina John"
+          imgDescrip="Arusha"
+        ></Card>
+        <Card
+          className="card-section_container"
+          img={Slider1}
+          imgTitle="Sarafina John"
+          imgDescrip="Arusha"
+        ></Card>
+      </div>
+      <div className="SeeAll-Btn-sm">
+        <Button type="primary" shape="round" size="large">
+          See All
+        </Button>
+      </div>
     </>
   );
 };
