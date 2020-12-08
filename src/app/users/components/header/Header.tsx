0fd92dc1250d5e5a133d41102ee212ba3fa1f561
nelsonfrank <nelsonfr78@outlook.com>
2020-12-08 12:00:42 +0300

    feat: Change picture format to webp
    Fixes #60

--- src/app/users/components/header/Header.tsx
+++ src/app/users/components/header/Header.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import { Carousel } from 'antd';
 
 //Images
-import Slider1 from '../../../../assets/images/slider-1.jpg';
-import Slider2 from '../../../../assets/images/slider-2.jpg';
-import Slider3 from '../../../../assets/images/slider-3.jpg';
-import Slider4 from '../../../../assets/images/slider-4.jpg';
-import Slider5 from '../../../../assets/images/slider-5.jpg';
-import Slider6 from '../../../../assets/images/slider-6.jpg';
+import Slider1 from '../../../../assets/images/slider-1.webp';
+import Slider2 from '../../../../assets/images/slider-2.webp';
+import Slider3 from '../../../../assets/images/slider-3.webp';
+import Slider4 from '../../../../assets/images/slider-4.webp';
+import Slider5 from '../../../../assets/images/slider-5.webp';
+import Slider6 from '../../../../assets/images/slider-6.webp';
 
 // Styles
 import './Header.less';
