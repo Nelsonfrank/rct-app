7594a6ab967cfd997d2ad381ba05031126a5f6a6
nelsonfrank <nelsonfr78@outlook.com>
2020-12-08 11:47:13 +0300

    feat: Add footer
    Fixes #63

---
+++ src/app/users/components/footer/Footer.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+
+// Styles
+import './Footer.less';
+// export interface FooterProps {}
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <h6>About</h6>
+            <p className="text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+              recusandae earum sequi vero blanditiis aliquid impedit eos aut vel
+              reprehenderit velit sunt natus voluptates iure, facere neque nemo
+              non culpa.
+            </p>
+          </div>
+
+          <div className="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul className="footer-links">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Tender request</a>
+              </li>
+              <li>
+                <a href="#">Tender Bid</a>
+              </li>
+              <li>
+                <a href="#">Sellers</a>
+              </li>
+              <li>
+                <a href="#">Buyer</a>
+              </li>
+              <li>
+                <a href="#">Feedback</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul className="footer-links">
+              <li>
+                <a href="about/">About Us</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Contribute</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Sitemap</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-sm-6 col-xs-12">
+              <p className="copyright-text">
+                Copyright &copy; 2017 All Rights Reserved by
+                <a href="#">Scanfcode</a>.
+              </p>
+            </div>
+
+            <div className="col-md-4 col-sm-6 col-xs-12">
+              <ul className="social-icons">
+                <li>
+                  <a className="facebook" href="#">
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a className="twitter" href="#">
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a className="dribbble" href="#">
+                    <i className="fa fa-dribbble"></i>
+                  </a>
+                </li>
+                <li>
+                  <a className="linkedin" href="#">
+                    <i className="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
