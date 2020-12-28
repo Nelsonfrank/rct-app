530d4d2aadaaa233c5c8ec006f67d12a79b99862
nelsonfrank <nelsonfr78@outlook.com>
2020-12-28 18:33:10 +0300

    feat: Modify create platform component

--- src/app/Management/pages/platform/components/create-platform-form/CreatePlatformForm.tsx
+++ src/app/Management/pages/platform/components/create-platform-form/CreatePlatformForm.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
 // dependence
-import { Input, InputNumber, Select, Radio, Button } from 'antd';
+import { Input, InputNumber, Select, Button } from 'antd';
 import { RouteComponentProps } from '@reach/router';
 // Styles
 import './CreatePlatform.less';
+
+import { region } from './country-dial';
 // export interface CreatePlatformFormProps {}
 
 const { Option } = Select;
 const CreatePlatformForm: React.FC<RouteComponentProps> = () => {
+  const [countrySelected, setcountrySelected] = React.useState('');
+
+  const handleOnSelectChange = (value: any) => {
+    setcountrySelected(value);
+    console.log(countrySelected);
+  };
   return (
     <>
       <div className="create-platform-container">
-        <h1 className="create-platform-header">Create Platform Form</h1>
+        <h1 className="create-platform-header">Create Platform</h1>
         <hr className="platform-divider" />
-        <h3>Platform Information</h3>
+        <h3 className="platform-info">Platform Information</h3>
         <hr className="platform-divider" />
         <div>
           <Input placeholder="Platform Name" size="large" />
@@ -26,16 +34,20 @@ const CreatePlatformForm: React.FC<RouteComponentProps> = () => {
           <div className="platform-location-options">
             <Select
               size="large"
-              defaultValue="region"
+              showSearch
               style={{ width: '100%' }}
+              placeholder="Region"
+              optionFilterProp="children"
+              filterOption={(input, option: any) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+              onChange={(e) => handleOnSelectChange(e)}
             >
-              <Option value="region">Region</Option>
-              <Option value="jack">Jack</Option>
-              <Option value="lucy">Lucy</Option>
-              <Option value="disabled" disabled>
-                Disabled
-              </Option>
-              <Option value="Yiminghe">yiminghe</Option>
+              {region.map((item) => (
+                <Option key={item.city} value={item.city}>
+                  {item.city}
+                </Option>
+              ))}
             </Select>
           </div>
         </div>
@@ -53,18 +65,15 @@ const CreatePlatformForm: React.FC<RouteComponentProps> = () => {
               </div>
             </div>
             <div className="create-platform-location">
-              <div className="platform-location-options">
+              <div
+                className="platform-location-options"
+                style={{ width: '100%' }}
+              >
                 <InputNumber
                   placeholder="Phone Number"
                   size="large"
                   style={{ width: '100%' }}
                 />
-              </div>
-              <div className="platform-location-options">
-                <Radio.Group>
-                  <Radio value={'male'}>Male</Radio>
-                  <Radio value={'female'}>Female</Radio>
-                </Radio.Group>
               </div>
             </div>
           </div>
