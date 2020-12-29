be38ee0f411d0abe1cd2e02ab5a45c9449048e31
nelsonfrank <nelsonfr78@outlook.com>
2020-12-29 17:15:45 +0300

    fix; Change price data type from number to striing In order to enable sorter function to work

--- src/app/Management/pages/dashboard/Dashboard.tsx
+++ src/app/Management/pages/dashboard/Dashboard.tsx
@@ -76,54 +76,54 @@ const price = [
   {
     id: '1',
     variety: 'Kiyela',
-    price: 2000,
+    price: '2000',
     region: 'Mbeya',
   },
   {
     id: '2',
     variety: 'Mbeya',
-    price: 2500,
+    price: '2500',
     region: 'Mbeya',
   },
   {
     id: '3',
     variety: 'Shinyanga',
-    price: 2300,
+    price: '2300',
     region: 'Shinyanga',
   },
   {
     id: '4',
     variety: 'Magugu',
-    price: 2300,
+    price: '2300',
     region: 'Arusha',
   },
   {
     id: '5',
     variety: 'Morogoro',
-    price: 2300,
+    price: '2300',
     region: 'Mbeya',
   },
   {
     id: '6',
     variety: 'Mbeya',
-    price: 2300,
+    price: '2300',
     region: 'Mbeya',
   },
   {
     id: '7',
     variety: 'Kilombero',
-    price: 2300,
+    price: '2300',
     region: 'Morogoro',
   },
   {
     id: '8',
     variety: 'Ruvuma',
-    price: 2300,
+    price: '2300',
     region: 'Ruvuma',
   },
 ];
 const PriceRate = (
-  data: { variety: string; price: number; region: string }[],
+  data: { variety: string; price: string; region: string }[],
   style?: React.CSSProperties,
 ) => {
   const columns = [
@@ -143,7 +143,7 @@ const PriceRate = (
       key: 'price',
       sorter: {
         // eslint-disable-next-line
-        compare: (a: any, b: any) => a.price.length - b.price.length,
+        compare: (a: any, b: any) => a.price - b.price,
       },
     },
     {
