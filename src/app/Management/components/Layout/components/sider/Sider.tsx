ef0b15aa7d7d916e9fb485295074eaa91e317271
nelsonfrank <nelsonfr78@outlook.com>
2021-01-28 08:25:27 +0300

    feat: Add side bar options

--- src/app/Management/components/Layout/components/sider/Sider.tsx
+++ src/app/Management/components/Layout/components/sider/Sider.tsx
@@ -9,10 +9,12 @@ import {
   FileOutlined,
   TeamOutlined,
   AccountBookOutlined,
-  // MessageOutlined,
+  MessageOutlined,
   BlockOutlined,
   UpSquareOutlined,
   AppstoreAddOutlined,
+  ControlOutlined,
+  PlayCircleOutlined,
 } from '@ant-design/icons';
 import { Link } from '@reach/router';
 //Logo
@@ -95,10 +97,16 @@ const Navigation: React.FC<NavigationProps> = (props: NavigationProps) => {
         <Menu.Item key="9" icon={<AppstoreAddOutlined />}>
           <Link to="tender-given">Tender Given</Link>
         </Menu.Item>
-        {/* <Menu.Item key="10" icon={<MessageOutlined />}>
-          <Link to="/dashboard">Feedback</Link>
-        </Menu.Item> */}
-        <Menu.Item key="11" icon={<FileOutlined />}>
+        <Menu.Item key="10" icon={<ControlOutlined />}>
+          <Link to="dashboard">Admins</Link>
+        </Menu.Item>
+        <Menu.Item key="11" icon={<PlayCircleOutlined />}>
+          <Link to="dashboard">Bunners</Link>
+        </Menu.Item>
+        <Menu.Item key="12" icon={<MessageOutlined />}>
+          <Link to="dashboard">Feedback</Link>
+        </Menu.Item>
+        <Menu.Item key="13" icon={<FileOutlined />}>
           <Link to="logs">Logs</Link>
         </Menu.Item>
       </Menu>
